80205052bc2cd1163e992d9f55e13086e35922ca
Akula Ravikanth <akula.ravikanth@invenics.com>
2025-02-10 13:12:09 +0530

    Configured Public and Private subnets

--- main.tf
+++ main.tf
@@ -4,3 +4,27 @@ resource "aws_vpc" "main-vpc" {
     Name = "main"
   }
 }
+
+resource "aws_subnet" "public_subnets" {
+  vpc_id                  = aws_vpc.main-vpc.id
+  count                   = length(var.public_subnets)
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zones[count.index]
+
+  tags = {
+    Name = "public-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  vpc_id            = aws_vpc.main-vpc.id
+  count             = length(var.private_subnets)
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "private-${count.index + 1}"
+  }
+
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,18 @@ variable "vpc_cidr_block" {
   type    = string
   default = "10.0.0.0/16"
 }
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
